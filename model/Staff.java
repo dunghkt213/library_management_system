7602023c989c1c6d15cc77920486f432ddc6adab
dunghkt213 <dung1990hk@gmail.com>
2024-10-18 08:40:17 +0700

    create model

---
+++ model/Staff.java
@@ -0,0 +1,66 @@
+package model;
+
+public class Staff {
+    private int staffId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String position;
+
+    public Staff(int staffId, String firstName, String lastName, String email, String phoneNumber, String position) {
+        this.staffId = staffId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.position = position;
+    }
+    public int getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(int staffId) {
+        this.staffId = staffId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
