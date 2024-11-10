eaf27e62a1d1e20398fc50cd45186113f87c2514
23021473vuvietanh <23021473@vnu.edu.vn>
2024-11-10 10:35:34 +0700

    last

--- Lib_management_system_new/src/main/java/viewbook/viewbookcontroller.java
+++ Lib_management_system_new/src/main/java/viewbook/viewbookcontroller.java
@@ -1,7 +1,5 @@
-package manage;
+package viewbook;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,7 +11,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
-public class managebookcontroller {
+public class viewbookcontroller {
     @FXML
     private TextField textField;
 
